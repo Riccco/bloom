5e2f4777d98a4658a515f7cbebb5a7c510356a4f
rico <5924435@qq.com>
2021-07-04 22:07:34 +0800

    创建tabs

---
+++ src/components/TabsDemo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Tabs示例</div>
+    <h1>示例1</h1>
+    <tabs >
+        <tab>内容一</tab>
+        <tab>内容二</tab>
+    </tabs>
+</template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+export default {
+    
+}
+</script>
